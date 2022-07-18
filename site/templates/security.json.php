37388840f865984671396e006d20cf38b16dc2c3
Lukas Bestle <lukas@getkirby.com>
2022-07-18 09:45:54 +0200

    Security: New `Messages` field

--- site/templates/security.json.php
+++ site/templates/security.json.php
@@ -10,7 +10,8 @@ $idFilter = function ($entry) {
 $data = [
     'latest'    => $kirby->version(),
     'versions'  => array_values($page->versions()->toArray($idFilter)),
-    'incidents' => array_values($page->incidents()->toArray($idFilter))
+    'incidents' => array_values($page->incidents()->toArray($idFilter)),
+    'messages'  => array_values($page->messages()->toArray($idFilter))
 ];
 
 echo json($data);
